1c46963ba114aea1b55849c9e7cb7884bce3a0df
Micky Hall <m172412@usna.edu>
2016-11-09 17:04:55 -0800

    commenting

--- part2/phrasehashwhich_language.go
+++ part2/phrasehashwhich_language.go
@@ -10,88 +10,107 @@ import (
   "strings"    // Used to split a phrase up into seperate words
 )
 
-func check(e error) {
-    if e != nil {
-        panic("Invalid Arguments!\n usage: go run which_lang.go <text file>")
-    }
-}
 
+/*
+This function will be spanwed as a go routine(also known as a thread) every time it is called.
+1) It will open up the dictionary that it is passed
+2) It will loop through every word in the dictionary and see if it exist in the map(hash table) of the text file you submitted
+3) For every word that matches between the dictionary and the map, the length of the word is added to a counting variable
+4) The quality count that came from the matching words, and the name of the dictionary that was searched is passed back to main.
+*/
 func how_many_words(c chan [2]int,language string,phrase_map map[string]int,which_lang int){
 
   count := 0
+
+  // 1
   f, _ := os.Open(language)
   scanner := bufio.NewScanner(f)
   scanner.Split(bufio.ScanWords)
   for scanner.Scan(){
     line := scanner.Text()
+
+    // 2
     _, exist := phrase_map[line]
+
+    // 3
     if exist == true {
-        count++
+        count += len(line)
     }
   }
   f.Close()
 
+  // 4
   holder := [2]int{count, which_lang}
   c <- holder
 }
 
+/*
+The main function
+1) It validates that you entered enought arguments on the command line, and that you gave it a valid text file to read
+2) Reads in the file to array, and then sets that array as a hash table for faster lookup times later
+3) Reads in all of the dictionary file names in the dictionary directory
+4) Initializes variables need for the go routines
+5) Loops through all of the dictionary names and spawns a thread that will count how many words from your text file is in that dictionary
+6) It will collect all of the returned values from our go routines
+7) It will loop through all of the returned values and print the language that had the highest quality word count returned
+*/
 func main(){
-  //Channel variable for keeping track of max word count in a dictionary and which dictionary is being used
-  c := make(chan [2]int)
-  //An array to hold all of the dictionary names
 
-
+  // 1
+  if len(os.Args) != 2 {
+    panic("Not Enough Arguments!\n usage: go run phrasehashwhich_language.go <text file>")
+  }
   phrase, error := ioutil.ReadFile(os.Args[1])
-  check(error)
+  if error != nil{
+    panic("Invalid Argument!\n usage: go run phrasehashwhich_language.go <text file>")
+  }
 
+  // 2
   phrase_array := strings.Fields(string(phrase))
   phrase_map := make(map[string]int)
-
   for i:=0; i<len(phrase_array);i++{
       phrase_map[phrase_array[i]] = 1
   }
 
-  var langs_string [18]string
-  //An array to match the string value of the dictionary to an int
-  var all_langs [18]int
-  //A counter to keep track of how many dictionaries we have
-  count := 0
-  //Opens the dictionaries directory
+  // 3
   files,_ := ioutil.ReadDir("./dictionaries")
 
-  //phrase := "Sólo tú le das brillo y amor a mi corazón. Sólo tú me miras con tanta pureza y belleza interior.Sólo tú llenas de alegría a mi alma.Sólo tú conoces mis secretos, deseos y anhelos.Sólo tú me transmites calor con tus manos.Sólo tú me das fuerzas para seguir viviendo.Sólo tú me hablas con tanta dulzura.Sólo tú me escuchas con tanta paciencia.Sólo tú me acaricias con tanta delicadeza.Sólo tú me besas con tanta ternura.Sólo tú me comprendes con tanta sutileza.Sólo tú haces que me sienta el ser más feliz del universo.Sólo tú me das tanta seguridad en mí misma.Sólo tú me calmas y tranquilizas cuando estoy mal.Sólo tú eres el que puede entrar en mi corazón y en mi ser.Sólo tú me abrigas cuando tengo frío.Sólo tú eres y serás el dueño de mi corazón.Sólo tú eres mi sueño hecho realidad.Sólo tú eres y serás el que me ama y me amará siempre.Sólo tú eres al que amo y amaré eternamente!"
-  //Loops through all the files in the dictionary directory
-  for _,file := range files {
+  // 4
+  var langs_string [18]string
+  var all_langs [18]int
+  count := 0
+  max := 0
 
-    //Adds the name of all of the files to the array
+  // A channel variable is a variable that acts as a pipe between a go routine(thread) and the main function
+  c := make(chan [2]int)
+
+  // 5
+  for _,file := range files {
     langs_string[count]=file.Name()
-    //Creates the string that will be sent to our function for opening the file
     language := "dictionaries/" + file.Name()
-    //Calls our function to count the words in that file
     go how_many_words(c,language,phrase_map,count)
-    //Increment the number of dictionaries we have started searching through
     count++
   }
 
-  //Collects the values returned from our channels, which will be the max count of words in that dictionary
+  // 6
   for i:=0;i < len(files); i++{
     holder := <-c
     all_langs[holder[1]] = holder[0]
   }
 
-  max := 0
-  //Loops through all of the maxes that we recieved and print's out the greatest one
+  // 7
   for j := 0; j < len(files); j++{
-
     if all_langs[j] > max{
       max = all_langs[j]
     }
   }
-
-  for i:= 0; i < len(files); i++{
-    if all_langs[i] == max{
-
-      fmt.Println(langs_string[i], "is the language you are looking for")
+  if max == 0{
+    fmt.Println("We do not have a dictionary that matches this language")
+  } else {
+    for i:= 0; i < len(files); i++{
+      if all_langs[i] == max{
+        fmt.Println(langs_string[i], "is the language you are looking for")
+      }
     }
   }
 }
